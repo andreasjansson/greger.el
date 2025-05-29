eabeb9ba68ffbb859eab2cdd93ac1e1fa3a5bcce
andreasjansson <andreas@replicate.ai>
2025-05-29 13:42:50 +0200

    Update greger-tools--replace-function to pass buffer to git-stage-and-commit

--- greger-stdlib.el
+++ greger-stdlib.el
@@ -611,7 +611,7 @@ If BUFFER is provided, it will be staged and committed along with the modified f
      (save-buffer))
 
     ;; Stage and commit the file
-    (let ((git-result (greger-tools--git-stage-and-commit (list expanded-path) commit-message)))
+    (let ((git-result (greger-tools--git-stage-and-commit (list expanded-path) commit-message buffer)))
       (format "Successfully replaced function '%s' in %s. %s"
               function-name expanded-path git-result))))
 
