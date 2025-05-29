8dedb597a73fc75a91a21bb499a2b329a6daf905
andreasjansson <andreas@replicate.ai>
2025-05-29 13:57:32 +0200

    Update greger-tools--eval-elisp-defuns to work in background without split window

--- greger-stdlib.el
+++ greger-stdlib.el
@@ -838,9 +838,7 @@ If BUFFER is provided, it will be staged and committed along with the modified f
     (when (file-directory-p expanded-path)
       (error "Path is a directory, not a file: %s" expanded-path))
 
-    (greger-tools--with-split-window
-     (find-file expanded-path)
-
+    (with-current-buffer (find-file-noselect expanded-path)
      ;; Navigate to and evaluate each function
      (dotimes (i (length function-names))
        (let ((function-name (aref function-names i)))
