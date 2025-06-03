8fa7ca7a2ba50a15b88bd84161e622929b75d055
andreasjansson <andreas@replicate.ai>
2025-06-03 12:34:24 +0200

    Add metadata to greger-pkg.el for MELPA compliance

--- greger-pkg.el
+++ greger-pkg.el
@@ -1,4 +1,8 @@
 (define-package "greger" "0.1.0"
   "Chat with language models"
   '((emacs "28.0")
-    (markdown-mode "2.3")))
+    (markdown-mode "2.3"))
+  :keywords '("ai" "chat" "language-models" "tools")
+  :url "https://github.com/andreasjansson/greger.el"
+  :maintainer '("Andreas Jansson" . "andreas@jansson.me.uk")
+  :authors '(("Andreas Jansson" . "andreas@jansson.me.uk")))
