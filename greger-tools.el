16aa251a2edd4329cfd1d3d5a2bc65e5ef4183ba
andreasjansson <andreas@replicate.ai>
2025-06-03 22:32:48 +0200

    Fix long line and quoting in greger-tools-execute docstring

--- greger-tools.el
+++ greger-tools.el
@@ -74,8 +74,8 @@ Example:
 (defun greger-tools-execute (tool-name args callback buffer &optional metadata)
   "Execute TOOL-NAME with ARGS and call CALLBACK with (result error).
 If the tool has :pass-buffer set, BUFFER will be passed to the tool function.
-If the tool has :pass-callback set, CALLBACK will be passed to the tool function
-instead of greger-tools-execute calling the callback with the result.
+If the tool has :pass-callback set, CALLBACK will be passed to the tool
+function instead of `greger-tools-execute' calling the callback with the result.
 If the tool has :pass-metadata set, METADATA will be passed to the tool function."
 
   (let ((tool-def (gethash tool-name greger-tools-registry)))
