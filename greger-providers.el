1ba03b7ce5f45f5592f5a1c177d158d9dc8285d9
andreasjansson <andreas@replicate.ai>
2025-06-03 13:19:48 +0200

    Add missing headers to greger-providers.el

--- greger-providers.el
+++ greger-providers.el
@@ -1,5 +1,10 @@
 ;;; greger-providers.el --- AI provider configurations -*- lexical-binding: t -*-
 
+;; Author: Andreas Jansson <andreas@jansson.me.uk>
+;; Version: 0.1.0
+;; URL: https://github.com/andreasjansson/greger.el
+;; Package-Requires: ((emacs "28.1"))
+
 ;;; Commentary:
 ;; Manages different AI provider configurations and request building
 
