7d8a19797bc41e28eabbc9a84f894642843269b3
andreasjansson <andreas@replicate.ai>
2025-06-03 13:19:00 +0200

    Add missing headers to greger-stream.el

--- greger-stream.el
+++ greger-stream.el
@@ -1,5 +1,10 @@
 ;;; greger-stream.el --- Streaming support for greger -*- lexical-binding: t -*-
 
+;; Author: Andreas Jansson <andreas@jansson.me.uk>
+;; Version: 0.1.0
+;; URL: https://github.com/andreasjansson/greger.el
+;; Package-Requires: ((emacs "28.1"))
+
 ;;; Commentary:
 ;; Handles streaming responses from AI providers
 
