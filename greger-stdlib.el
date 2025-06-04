4b62183ecf66169a06ba97f5e5c6c74ca2c3605d
andreasjansson <andreas@replicate.ai>
2025-06-04 11:13:05 +0200

    Fix recursive list-directory to use proper file filtering

--- greger-stdlib.el
+++ greger-stdlib.el
@@ -483,7 +483,7 @@ PREFIX is used internally for nested directory structure."
                (relative-path (file-relative-name file (expand-file-name path))))
           (when (and (not (string= basename "."))
                      (not (string= basename ".."))
-                     (not (string-match-p exclude-pattern relative-path)))
+                     (greger-stdlib--should-include-file-p relative-path exclude-pattern))
             (let ((formatted (greger-stdlib--format-file-info file basename exclude-pattern)))
               (when formatted
                 (push formatted current-results))))))
