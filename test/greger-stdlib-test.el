ddedd22d964267d76f1f43bbfb9dfba1879e441d
andreasjansson <andreas@replicate.ai>
2025-06-18 12:08:03 +0200

    Update shell-command test calls to pass all required arguments (2/9)

--- test/greger-stdlib-test.el
+++ test/greger-stdlib-test.el
@@ -130,7 +130,11 @@ This ensures the '..' entry has predictable permissions in tests."
       (greger-stdlib--shell-command
        "echo 'apple\nbanana\ncherry' | grep 'an'"
        (lambda (output err)
-         (setq result output error err callback-called t)))
+         (setq result output error err callback-called t))
+       "."  ; working-directory
+       nil  ; timeout
+       nil  ; enable-environment
+       nil) ; metadata
 
       ;; Wait for async operation to complete
       (let ((timeout 0))
