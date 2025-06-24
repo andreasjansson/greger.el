9e573ab18f49a892c0aca4386e920eda88fab947
Andreas Jansson <andreas@replicate.ai>
2025-06-24 08:50:52 +0200

    Address initial melpa issues (#66)
    * Use default recipe
    
    * Better Commentary

--- greger.el
+++ greger.el
@@ -28,7 +28,10 @@
 ;; SOFTWARE.
 
 ;;; Commentary:
-;; This package provides an interface for interacting with AI language models
+;; Agentic coding environment using Claude and a suite of built-in tools.
+;; Capable of viewing and editing files, searching code, listing files,
+;; searching the web and downloading web pages.  Extensible with custom
+;; tools defined in Emacs Lisp.
 
 ;;; Code:
 
