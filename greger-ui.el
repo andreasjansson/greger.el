5239f2f0a5e2e285149aaa7749ae89d2d32a093b
andreasjansson <andreas@replicate.ai>
2025-06-25 08:38:44 -0700

    Update tool result id invisibility function to properly handle folding mode toggle

--- greger-ui.el
+++ greger-ui.el
@@ -180,10 +180,9 @@ NODE is the matched tree-sitter node"
 
 (defun greger-ui--make-tool-result-id-invisible (node _override _start _end)
   "Make id NODE invisible while preserving face styling."
-  (when greger-ui-folding-mode
-   (let ((node-start (treesit-node-start node))
-         (node-end (treesit-node-end node)))
-     (put-text-property node-start node-end 'invisible t))))
+  (let ((node-start (treesit-node-start node))
+        (node-end (treesit-node-end node)))
+    (put-text-property node-start node-end 'invisible greger-ui-folding-mode)))
 
 (defun greger-ui--make-tool-use-id-invisible (node _override _start _end)
   "Make id NODE invisible while preserving face styling."
