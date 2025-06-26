b31a012efc7fd7af04988e2f8c66355d988b03a4
andreasjansson <andreas@replicate.ai>
2025-06-26 08:00:28 -0700

    Debug setcar behavior

---
+++ debug-setcar.el
@@ -0,0 +1,19 @@
+;;; Debug setcar behavior
+
+(defun test-setcar ()
+  (let* ((original-list '((a . 1) (b . 2) (c . 3)))
+         (current-list (cdr original-list))) ; Points to ((b . 2) (c . 3))
+    
+    (message "Original list: %S" original-list)
+    (message "Current list: %S" current-list)
+    (message "Car of current list: %S" (car current-list))
+    
+    ;; Modify the car of current-list
+    (setcar current-list (cons '(new . "value") (car current-list)))
+    
+    (message "After setcar:")
+    (message "Original list: %S" original-list)
+    (message "Current list: %S" current-list)
+    (message "Car of current list: %S" (car current-list))))
+
+(test-setcar)
