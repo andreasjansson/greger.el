9b58289503de6db20c34c0ae7f3243775a259373
andreasjansson <andreas@replicate.ai>
2025-06-26 11:34:16 -0700

    add flycheck-emacs-lisp-load-path to dir locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((emacs-lisp-mode
-  (package-lint-main-file . "greger.el")))
+  (package-lint-main-file . "greger.el")
+  (add-to-list 'flycheck-emacs-lisp-load-path "~/projects/greger.el")
