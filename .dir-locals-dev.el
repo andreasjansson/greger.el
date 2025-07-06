88cf2c378b726f68718720a72c325b8b1835e8d7
andreasjansson <andreas@replicate.ai>
2025-07-06 15:45:10 +0200

    Update development config template for git branch approach

--- .dir-locals-dev.el
+++ .dir-locals-dev.el
@@ -1,19 +1,20 @@
-;;; .dir-locals-dev.el --- Local development configuration for greger.el -*- lexical-binding: t -*-
+;;; .dir-locals-dev.el --- Development configuration for greger.el -*- lexical-binding: t -*-
 
 ;; This file contains development-specific configuration for greger.el
-;; Copy this to .dir-locals.el to enable local development settings
+;; Copy this to .dir-locals.el to enable development branch settings
 ;; 
 ;; Usage:
 ;;   1. Copy this file to .dir-locals.el:
 ;;      cp .dir-locals-dev.el .dir-locals.el
-;;   2. Restart Emacs or run M-x revert-buffer in greger.el files
-;;   3. The local grammar will be automatically used for development
+;;   2. Update the branch name below to your development branch
+;;   3. Restart Emacs or run M-x revert-buffer in greger.el files
+;;   4. Run M-x greger-install-grammar to install from your branch
 
-((nil . ((greger-local-grammar-path . "../greger-grammar"))))
+((nil . ((greger-local-grammar-path . "your-development-branch-name"))))
 
 ;; Alternative: You can also set this in your init.el or interactively:
-;; (setq greger-local-grammar-path (expand-file-name "../greger-grammar"))
+;; (setq greger-local-grammar-path "your-branch-name")
 ;; or
-;; M-x customize-variable RET greger-local-grammar-path RET
+;; M-x greger-set-grammar-branch RET your-branch-name RET
 
 ;;; .dir-locals-dev.el ends here
